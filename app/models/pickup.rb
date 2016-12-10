952d1dda8eef53f8ffb28b47beea72cf26ffc2da
Raghu Betina <raghu@firstdraft.com>
2016-12-10 00:57:05 +0000

    Validate presence of pickups.image_upload

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -5,6 +5,8 @@ class Pickup < ApplicationRecord
 
   # Validations
 
+  validates :image_upload, :presence => true
+
   validates :package_destination, :presence => true
 
   validates :user_id, :presence => true
